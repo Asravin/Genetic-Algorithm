f954703ee4200ba54ee7e5f6df53da11efd4b747
Asravin <maisweedfell@gmail.com>
2024-05-21 13:13:40 +0300

    Функция генерации случайной хромасомы

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -5,4 +5,13 @@ class Chromosome:
         self.rating = 0
         self.size = size
         self.genes = bytearray(size)
-        
+        if gene_pool is not None:
+            self.set_random_genes(gene_pool)
+    
+    
+    def set_random_genes(self, gene_pool):
+        gene_pool_range = len(gene_pool) - 1
+        for i in range(self.size):
+            rand_pos = randint(0, gene_pool_range)
+            self.genes[i] = gene_pool(rand_pos)
+            
